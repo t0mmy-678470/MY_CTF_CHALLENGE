72439a5a970ffe692be4ba4f05c29fc73904db7a
t0mmy-678470 <guoyihong64@gmail.com>
2024-10-09 05:50:04 +0000

    modify md5_collision/chall.py

--- md5_collision/chall.py
+++ md5_collision/chall.py
@@ -8,12 +8,13 @@ try:
 except:
     print('Please input hex')
     exit()
+
 hash1 = hashlib.md5(b1).hexdigest()
 hash2 = hashlib.md5(b2).hexdigest()
+
 if hex1 != hex2 and hash1 == hash2:
     print("How did you do that?")
     print(os.getenv('FLAG'))
 else:
     print(f'{hash1} != {hash2}')
     print("keep trying")
-    print(os.getenv('FLAG'))
